7df9a085ee4132a8c9c357ab9b23cf061f529a26
Vaibhav Aher <vaibhavva219@gmail.com>
2023-08-24 17:45:08 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
       id='home'
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container  '
     >
-      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+      <div className='relative xl:w-2/5 flex flex-col justify-center w-full  max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>
           Our Summer collections
         </p>

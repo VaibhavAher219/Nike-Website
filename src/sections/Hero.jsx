604660168288aca18f4f24eb2deca003261b0aac
Vaibhav Aher <vaibhavva219@gmail.com>
2023-08-24 17:08:59 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
     >
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>
-          Our Summer collections
+          Our Winter collections
         </p>
 
         <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
